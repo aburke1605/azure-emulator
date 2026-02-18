0dae56e246889f7674610e72029a6c59f11711fe
aburke1605 <aodhanburke@hotmail.com>
2026-02-18 17:55:10 +1300

    argparse

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
-print("success")
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--test",
+    type=str,
+    required=True
+)
+args = parser.parse_args()
+
+print(args.test)
 
