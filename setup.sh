1b2747afb57ca6399f18a000bc53329a1d7085fe
aburke1605 <aodhanburke@hotmail.com>
2026-02-19 10:48:02 +1300

    create venv so user doesn't have to activate

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
-alias azemu='python $PWD/run.py'
+# create venv
+if [ ! -d $PWD/.venv ]; then
+  python3 -m venv .venv
+  $PWD/.venv/bin/pip install -r requirements.txt
+fi
 
+alias azemu='$PWD/.venv/bin/python $PWD/run.py'
