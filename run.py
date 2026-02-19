aa2207db962477f520cdbe2c1296c29132924beb
aburke1605 <aodhanburke@hotmail.com>
2026-02-19 15:48:33 +1300

    remove variable names

--- run.py
+++ run.py
@@ -9,16 +9,15 @@ subparsers = parser.add_subparsers(
     required=True
 )
 
-parser_new = subparsers.add_parser(
+subparsers.add_parser(
     "new",
     help="create a new blank stack"
 )
 
-parser_init = subparsers.add_parser(
+subparsers.add_parser(
     "init",
     help="add default service(s) to the stack"
-)
-parser_init.add_argument(
+).add_argument(
     "services",
     type=str,
     nargs="+",
@@ -29,17 +28,18 @@ parser_init.add_argument(
     help="list of service names to add"
 )
 
-parser_up = subparsers.add_parser(
+subparsers.add_parser(
     "up",
     help="emulate deployment"
 )
 
-parser_down = subparsers.add_parser(
+subparsers.add_parser(
     "down",
     help="destroy emulator"
 )
 
 args = parser.parse_args()
+
 
 def new():
     yaml.safe_dump(
@@ -47,17 +47,21 @@ def new():
         open("compose.yml", "w")
     )
 
+
 def init():
     config = yaml.safe_load(open("compose.yml", "r"))
     if "database" in args.services:
         config["services"]["database"] = yaml.safe_load(open("templates/database.yml", "r"))
     yaml.safe_dump(config, open("compose.yml", "w"))
 
+
 def up():
     result = subprocess.run(["docker", "compose", "up", "--detach"])
 
+
 def down():
     result = subprocess.run(["docker", "compose", "down"])
+
 
 switcher = {
     "new": new,
