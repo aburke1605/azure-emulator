44747b8c49d2308d613dccad50241f4a50271092
aburke1605 <aodhanburke@hotmail.com>
2026-02-19 16:03:49 +1300

    use better pattern

--- run.py
+++ run.py
@@ -2,6 +2,29 @@ import argparse
 import subprocess
 import yaml
 
+
+def new(args):
+    yaml.safe_dump(
+        {"services": {}, "volumes": {}},
+        open("compose.yml", "w")
+    )
+
+
+def init(args):
+    config = yaml.safe_load(open("compose.yml", "r"))
+    if "database" in args.services:
+        config["services"]["database"] = yaml.safe_load(open("templates/database.yml", "r"))
+    yaml.safe_dump(config, open("compose.yml", "w"))
+
+
+def up(args):
+    result = subprocess.run(["docker", "compose", "up", "--detach"])
+
+
+def down(args):
+    result = subprocess.run(["docker", "compose", "down"])
+
+
 parser = argparse.ArgumentParser()
 
 subparsers = parser.add_subparsers(
@@ -12,7 +35,7 @@ subparsers = parser.add_subparsers(
 subparsers.add_parser(
     "new",
     help="create a new blank stack"
-)
+).set_defaults(func=new)
 
 subparsers.add_parser(
     "init",
@@ -26,47 +49,17 @@ subparsers.add_parser(
         "database",
     ],
     help="list of service names to add"
-)
+).set_defaults(func=init)
 
 subparsers.add_parser(
     "up",
     help="emulate deployment"
-)
+).set_defaults(func=up)
 
 subparsers.add_parser(
     "down",
     help="destroy emulator"
-)
+).set_defaults(func=down)
 
 args = parser.parse_args()
-
-
-def new():
-    yaml.safe_dump(
-        {"services": {}, "volumes": {}},
-        open("compose.yml", "w")
-    )
-
-
-def init():
-    config = yaml.safe_load(open("compose.yml", "r"))
-    if "database" in args.services:
-        config["services"]["database"] = yaml.safe_load(open("templates/database.yml", "r"))
-    yaml.safe_dump(config, open("compose.yml", "w"))
-
-
-def up():
-    result = subprocess.run(["docker", "compose", "up", "--detach"])
-
-
-def down():
-    result = subprocess.run(["docker", "compose", "down"])
-
-
-switcher = {
-    "new": new,
-    "init": init,
-    "up": up,
-    "down": down
-}
-switcher.get(args.command, lambda: None)()
+args.func(args)
